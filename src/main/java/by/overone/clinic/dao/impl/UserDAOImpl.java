87b499120db57ae17975c101d4e1971cca407f8f
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-12-02 20:14:39 +0300

    add connection

--- src/main/java/by/overone/clinic/dao/impl/UserDAOImpl.java
+++ src/main/java/by/overone/clinic/dao/impl/UserDAOImpl.java
@@ -3,9 +3,26 @@ package by.overone.clinic.dao.impl;
 import by.overone.clinic.dao.UserDAO;
 import by.overone.clinic.model.User;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDAOImpl implements UserDAO {
+    private static Connection connection;
+
+    static {
+        try {
+            String URL = "jdbc:mysql://localhost:3306/veterinary_clinic";
+            String dbUser = "root";
+            String password = "root";
+
+            connection = DriverManager.getConnection(URL, dbUser, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public List<User> getUsers() {
         return null;
