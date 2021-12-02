d32c8e523ce3da4dde7db7bda4d96d44f29a2e99
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-12-02 20:00:54 +0300

    add signatures of methods

--- src/main/java/by/overone/clinic/dao/UserDAO.java
+++ src/main/java/by/overone/clinic/dao/UserDAO.java
@@ -1,4 +1,17 @@
 package by.overone.clinic.dao;
 
+import by.overone.clinic.model.User;
+
+import java.util.List;
+
 public interface UserDAO {
+    List<User> getUsers();
+    User getUserById(long id);
+    User getUserByEmail(String email);
+
+    User addUser(User user);
+
+    User updateUser(User user);
+
+    User deleteUser(long id);
 }
--- src/main/java/by/overone/clinic/dao/impl/UserDAOImpl.java
+++ src/main/java/by/overone/clinic/dao/impl/UserDAOImpl.java
@@ -1,4 +1,38 @@
 package by.overone.clinic.dao.impl;
 
-public class UserDAOImpl {
+import by.overone.clinic.dao.UserDAO;
+import by.overone.clinic.model.User;
+
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return null;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User addUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User deleteUser(long id) {
+        return null;
+    }
 }
