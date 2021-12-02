c544505babda5bee44a82bc93cb3cec312208502
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-12-02 20:04:34 +0300

    add fields for User

--- src/main/java/by/overone/clinic/model/User.java
+++ src/main/java/by/overone/clinic/model/User.java
@@ -1,4 +1,13 @@
 package by.overone.clinic.model;
 
+import lombok.Data;
+
+@Data
+
 public class User {
+    private long id;
+    private String login;
+    private String password;
+    private String email;
+
 }
